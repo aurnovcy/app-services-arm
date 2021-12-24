4e90cb5ff6974c9afa4d0bd806334db6c78511fd
Aurnov Chattopadhyay <aurnovcy@gmail.com>
2021-12-23 16:39:25 -0800

    Update deployAppService.ps1

--- artifacts/deployAppService.ps1
+++ artifacts/deployAppService.ps1
@@ -2,5 +2,5 @@ Start-Transcript -Path C:\Temp\deployAppService.log
 
 Write-Host "Creating App Service plan. Hold tight, this might take a few minutes..."
 Write-Host "`n"
-$customLocationId = $(az customlocation show --name "tailwind-cl" --resource-group $env:resourceGroup --query id -o tsv)
-az appservice plan create --resource-group $env:resourceGroup --name TailwindQA --custom-location $customLocationId --per-site-scaling --is-linux --sku K1
+$customLocationId = $(az customlocation show --name "lucerne-cl" --resource-group $env:resourceGroup --query id -o tsv)
+az appservice plan create --resource-group $env:resourceGroup --name LucerneFeedback --custom-location $customLocationId --per-site-scaling --is-linux --sku K1
